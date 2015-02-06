93474c9b8d68b8f4386135d3dabc808daece1b97
Robert Munoz <rmunoz527@gmail.com>
2015-02-05 20:17:26 -0800

    Initial setup of KNN Classifier

---
+++ KNNClassifier/KNNClassifier/KNNClassifier.py
@@ -0,0 +1,29 @@
+class KNNClassifier():
+    def __init__():None;
+
+def Predict():None;
+
+def Train():None;
+
+"""
+Receiver Operator Characteristic
+"""
+def ROC():None;
+
+def SetK():None;
+
+def SetClasses():None;
+
+def PredictSoft():None;
+
+def Classify():None;
+
+def GetK():None;
+
+def GetClasses():None;
+
+def Display():None;
+
+def ConfusionMatrix():None;
+
+def AreaUnderROC():None;
